52c04243279d4562f7b2b3d7ba19596ac908681a
mgunduz1 <mgunduz114118@gmail.com>
2021-04-26 12:20:55 +0300

    add event model and associations bween user

---
+++ db/migrate/20210523121521_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.references :user
+      t.string :title
+      t.string :location
+      t.text :description
+      t.date :date
+      t.integer :attend_limit
+      t.timestamps
+    end
+  end
+end
