a1b8b5a037c1b1c11181ddeb3badeafc6236474d
mgunduz1 <mgunduz114118@gmail.com>
2021-04-28 10:06:36 +0300

    add scope and root page for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :attendees
+
+  scope :expired, -> { where('date < ?', Date.today) }
+  scope :future, -> { where('date >= ?', Date.today) }
 end
