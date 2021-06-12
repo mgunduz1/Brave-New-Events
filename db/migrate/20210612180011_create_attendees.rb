bec61d6b3cdca0b52462662fe89ed42f2350e29e
mgunduz1 <mgunduz114118@gmail.com>
2021-06-12 21:05:28 +0300

    create new db fix sr errors

---
+++ db/migrate/20210612180011_create_attendees.rb
@@ -0,0 +1,11 @@
+class CreateAttendees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :attendees do |t|
+      t.references :event
+      t.references :user
+      t.boolean :will_join,  default: true
+
+      t.timestamps
+    end
+  end
+end
